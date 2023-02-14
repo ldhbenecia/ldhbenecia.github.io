d553dbe3e10ecaab2bac5350f706df527b723fad
DongHyeok Lim <jh07050@gmail.com>
2023-02-14 18:46:57 +0900

    Style: CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-
-  variants: {
-    extends: {},
+  theme: {
+    extend: {
+      keyframes: {
+        appear: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        boxShadow: {
+          default: "4px 12px 30px 6px rgba(0, 0, 0, 0.09)",
+        },
+      },
+      fontFamily: {
+        nanumLight: ["NanumLight", "sans-serif"],
+        nanumRegular: ["NanumRegular", "sans-serif"],
+        nanumBold: ["NanumBold", "sans-serif"],
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  plugins: [],
 }
