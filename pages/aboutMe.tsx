ca9ea2799a3932923332d3d815d42173c10d632c
DongHyeok Lim <jh07050@gmail.com>
2023-04-08 21:22:13 +0900

    Feat: components

---
+++ pages/aboutMe.tsx
@@ -0,0 +1,8 @@
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>나에 대하여</h1>
+    </div>
+  )
+}
---
+++ pages/projects.tsx
@@ -0,0 +1,9 @@
+import Layout from "./components/layout";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>프로젝트 넣는 공간</h1>
+    </Layout>
+  );
+}
