267d5f403916b88eb1915111e1ef74d5d4c64553
DongHyeok Lim <jh07050@gmail.com>
2023-04-09 02:00:16 +0900

    Style: Building

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import DarkModeButton from "./darkModeButton";
+
 export default function Header() {
   return (
     <header className="text-gray-600 body-font">
@@ -18,23 +21,11 @@ export default function Header() {
           <span className="ml-3 text-xl">DongHyeok Lim</span>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">Main</a>
-          <a className="mr-5 hover:text-gray-900">Projects</a>
+          <Link href="/" className="mr-5 hover:text-gray-900">Main</Link>
+          <Link href="projects" className="mr-5 hover:text-gray-900">Projects</Link>
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
+        {/* 다크모드 버튼 작업 */}
+        <DarkModeButton />
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,11 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Layout from "./components/layout";
-
-const inter = Inter({ subsets: ["latin"] });
+import Layout from "@/components/layout";
+import Main from "@/components/Main/main";
 
 export default function Home() {
   return (
     <Layout>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Hi, there
-              <br className="inline-block lg:inline-block" />
-              Welcome to My Portfolio
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              포트폴리오 설명란
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Projects
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Etc
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
-        </div>
+        <Main />
       </section>
     </Layout>
   );
